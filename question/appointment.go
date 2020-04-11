34ec5e32b8ad24963a62d5a72b3cb123c6174826
Marcus Soll <git@msoll.eu>
2020-04-11 17:39:31 +0200

    Added new question type 'appointment'
    This new question type is optimised for finding a date for an
    appointment. The results are displayed as the best date as well as a
    table optimised for date finding.
    
    Options allow easily creation of many possible dates without specifying
    each date seperately.

---
+++ question/appointment.go
@@ -0,0 +1,406 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2020 Marcus Soll
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package question
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"log"
+	"math"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Top-Ranger/questiongo/registry"
+)
+
+var appointmentDateFormatRead = "2006-01-02"
+var appointmentDateFormatWrite = "Monday, 02.01.2006 15:04"
+var appointmentDateFormatID = "02.01.2006T15:04"
+
+func init() {
+	err := registry.RegisterQuestionType(FactoryAppointment, "appointment")
+	if err != nil {
+		panic(err)
+	}
+}
+
+// FactoryAppointment is the factory for appointment questions.
+func FactoryAppointment(data []byte, id string) (registry.Question, error) {
+	var a appointment
+	err := json.Unmarshal(data, &a)
+	if err != nil {
+		return nil, err
+	}
+	a.id = id
+
+	_, ok := registry.GetFormatType(a.Format)
+	if !ok {
+		return nil, fmt.Errorf("appointment: Unknown format type %s (%s)", a.Format, id)
+	}
+
+	fd, err := time.Parse(appointmentDateFormatRead, a.FirstDate)
+	if err != nil {
+		return nil, fmt.Errorf("appointment: can not parse '%s' - %s", a.FirstDate, err.Error())
+	}
+	ld, err := time.Parse(appointmentDateFormatRead, a.LastDate)
+	if err != nil {
+		return nil, fmt.Errorf("appointment: can not parse '%s' - %s", a.LastDate, err.Error())
+	}
+
+	if ld.Before(fd) {
+		return nil, fmt.Errorf("appointment: LastDate (%s) can not be before FirstDate (%s)", a.LastDate, a.FirstDate)
+	}
+
+	ld = ld.AddDate(0, 0, 1) // Add one day for the for-loop later
+
+	w := make(map[time.Weekday]bool)
+	for i := range a.Days {
+		day, ok := appointmentParseWeekday(a.Days[i])
+		if !ok {
+			return nil, fmt.Errorf("appointment: can not parse day of week '%s'", a.Days[i])
+		}
+		w[day] = true
+	}
+
+	t := make([][]int, 0)
+
+	for i := range a.Time {
+		tn := make([]int, 2)
+		split := strings.Split(a.Time[i], ":")
+		if len(split) != 2 {
+			return nil, fmt.Errorf("appointment: Can not parse '%s' as time", a.Time[i])
+		}
+		tn[0], err = strconv.Atoi(split[0])
+		if err != nil {
+			return nil, fmt.Errorf("appointment: Can not parse '%s' as time - %s", a.Time[i], err.Error())
+		}
+		tn[1], err = strconv.Atoi(split[1])
+		if err != nil {
+			return nil, fmt.Errorf("appointment: Can not parse '%s' as time - %s", a.Time[i], err.Error())
+		}
+
+		t = append(t, tn)
+	}
+
+	ignore := make([]time.Time, len(a.ExceptDays))
+
+	for i := range a.ExceptDays {
+		ignore[i], err = time.Parse(appointmentDateFormatRead, a.ExceptDays[i])
+		if err != nil {
+			return nil, fmt.Errorf("appointment: can not parse '%s' - %s", a.ExceptDays[i], err.Error())
+		}
+	}
+
+	a.dates = make([]appointmentDate, 0)
+	sort.Strings(a.Time)
+
+	for fd.Before(ld) {
+		for i := range t {
+			newTime := time.Date(fd.Year(), fd.Month(), fd.Day(), t[i][0], t[i][1], 0, 0, fd.Location())
+
+			add := w[newTime.Weekday()]
+			for ign := range ignore {
+				add = add && (newTime.Year() != ignore[ign].Year() || newTime.Month() != ignore[ign].Month() || newTime.Day() != ignore[ign].Day())
+			}
+
+			if add {
+				a.dates = append(a.dates, appointmentDate{
+					ID:      fmt.Sprintf("%s_%s", id, newTime.Format(appointmentDateFormatID)),
+					Display: newTime.Format(appointmentDateFormatWrite),
+					time:    newTime,
+				})
+			}
+		}
+		fd = fd.AddDate(0, 0, 1)
+	}
+
+	return &a, nil
+}
+
+var appointmentDayMap = map[string]time.Weekday{
+	"mon": time.Monday, "tue": time.Tuesday, "wed": time.Wednesday, "thu": time.Thursday, "fri": time.Friday, "sat": time.Saturday, "sun": time.Sunday,
+	"mo": time.Monday, "tu": time.Tuesday, "we": time.Wednesday, "th": time.Thursday, "fr": time.Friday, "sa": time.Saturday, "su": time.Sunday,
+	"monday": time.Monday, "tuesday": time.Tuesday, "wednesday": time.Wednesday, "thursday": time.Thursday, "friday": time.Friday, "saturday": time.Saturday, "sunday": time.Sunday,
+}
+
+func appointmentParseWeekday(day string) (time.Weekday, bool) {
+	day = strings.ToLower(day)
+	weekday, ok := appointmentDayMap[day]
+	return weekday, ok
+}
+
+func appointmentColour(answer string) string {
+	switch answer {
+	case "✓":
+		return "#5EFF5E"
+	case "👎":
+		return "#FFE75E"
+	case "X":
+		return "#FF5E66"
+	case "?":
+		return "#DBD9E2"
+	default:
+		return ""
+	}
+}
+
+func appointmentPoints(answer string) float64 {
+	switch answer {
+	case "✓":
+		return 1.0
+	case "👎":
+		return 0.25
+	case "X":
+		return -1.0
+	default:
+		return 0.0
+	}
+}
+
+var appointmentTemplate = template.Must(template.New("appointmentTemplate").Parse(`{{.Text}}<br>
+<p><label for="{{.ID}}_name">Name {{if .NameRequired}}(<em>required</em>){{else}}(<em>optional</em>){{end}}:</label> <input type="text" id="{{.ID}}_name" name="{{.ID}}_name" placeholder="Name" {{if .NameRequired}}required{{end}}></p>
+<table>
+<thead>
+<tr>
+<th></th>
+<th>✓ (yes)</th>
+<th>👎 (only if needed)</th>
+<th>X (no)</th>
+<th>? (can not say)</th>
+</tr>
+</thead>
+<tbody>
+{{range $i, $e := .Data }}
+<tr>
+<td>{{if $e.Disabled}}<s>{{else}}<strong>{{end}}{{$e.Display}}{{if $e.Disabled}}</s>{{else}}</strong>{{end}}</td>
+<td class="centre" bgcolor="#5EFF5E"><input type="radio" name="{{$e.ID}}" value="✓" {{if $e.Disabled}} disabled {{end}}></td>
+<td class="centre" bgcolor="#FFE75E"><input type="radio" name="{{$e.ID}}" value="👎" {{if $e.Disabled}} disabled {{end}}></td>
+<td class="centre" bgcolor="#FF5E66"><input type="radio" name="{{$e.ID}}" value="X" {{if $e.Disabled}} disabled {{end}}></td>
+<td class="centre" bgcolor="#DBD9E2"><input type="radio" name="{{$e.ID}}" value="?" {{if $e.Disabled}} disabled {{end}}></td>
+</tr>
+{{end}}
+</tbody>
+</table>
+`))
+
+var appointmentStatisticsTemplate = template.Must(template.New("appointmentStatisticsTemplate").Parse(`{{.Text}}
+<p><strong>Best Date:</strong> {{.Best}}</p>
+<details>
+<summary>detailed results</summary>
+<div style="width: 100%; overflow-x: auto;">
+<table style="overflow-x: auto;">
+<thead>
+<tr>
+<th></th>
+{{range $i, $e := .Dates }}
+<th>{{$e}}</th>
+{{end}}
+</tr>
+</thead>
+{{range $i, $e := .Data }}
+<tr>
+<td><strong>{{$e.Name}}</strong></td>
+{{range $I, $E := .Answers }}
+<td class="centre" {{if index $E 0}}bgcolor="{{index $E 0}}"{{end}}>{{index $E 1}}</td>
+{{end}}
+</tr>
+{{end}}
+<tr>
+<td class="th-cell"><strong>Points</strong></td>
+{{range $i, $e := .Points }}
+<td class="centre{{if eq $i $.BestNumber}} th-cell{{end}}">{{printf "%.2f" $e}}</td>
+{{end}}
+</tr>
+</table>
+</div>
+</details>
+`))
+
+type appointmentTemplateStruct struct {
+	ID           string
+	Text         template.HTML
+	NameRequired bool
+	Data         []appointmentDate
+}
+
+type appointmentStatisticsTemplateStruct struct {
+	Text       template.HTML
+	Best       string
+	Dates      []string
+	Data       []appointmentStatisticsTemplateStructInner
+	Points     []float64
+	BestNumber int
+}
+
+type appointmentStatisticsTemplateStructInner struct {
+	Name    string
+	Answers [][]string // [colour, value]
+}
+
+type appointmentDate struct {
+	ID       string
+	Display  string
+	Disabled bool
+	time     time.Time
+}
+
+type appointment struct {
+	Format              string
+	Text                string
+	NameRequired        bool
+	DisallowVotesInPast bool
+	FirstDate           string
+	LastDate            string
+	Days                []string
+	Time                []string
+	ExceptDays          []string
+
+	id    string
+	dates []appointmentDate
+}
+
+func (a appointment) GetID() string {
+	return a.id
+}
+
+func (a appointment) GetHTML() template.HTML {
+	f, _ := registry.GetFormatType(a.Format)
+
+	td := appointmentTemplateStruct{
+		ID:           a.id,
+		Text:         f.Format([]byte(a.Text)),
+		NameRequired: a.NameRequired,
+		Data:         make([]appointmentDate, len(a.dates)),
+	}
+
+	now := time.Now()
+
+	for i := range a.dates {
+		td.Data[i].ID = a.dates[i].ID
+		td.Data[i].Display = a.dates[i].Display
+		td.Data[i].Disabled = a.DisallowVotesInPast && a.dates[i].time.Before(now)
+	}
+
+	output := bytes.NewBuffer(make([]byte, 0))
+	err := appointmentTemplate.Execute(output, td)
+	if err != nil {
+		log.Printf("date: Error executing template (%s)", err.Error())
+	}
+	return template.HTML(output.Bytes())
+}
+
+func (a appointment) GetStatisticsHeader() []string {
+	s := make([]string, len(a.dates)+1)
+	s[0] = fmt.Sprintf("%s_name", a.id)
+	for i := range a.dates {
+		s[i+1] = a.dates[i].ID
+	}
+	return s
+}
+
+func (a appointment) GetStatistics(data []string) [][]string {
+	result := make([][]string, len(data))
+	for i := range data {
+		s := make([]string, len(a.dates)+1)
+		var results map[string]string
+		err := json.Unmarshal([]byte(data[i]), &results)
+		if err != nil {
+			log.Printf("appointment: Error unmarshalling %d - %s - %s", i, data[i], err.Error())
+			result[i] = s
+			continue
+		}
+		s[0] = results[fmt.Sprintf("%s_name", a.id)]
+		for j := range a.dates {
+			s[j+1] = results[a.dates[j].ID]
+		}
+	}
+	return result
+}
+
+func (a appointment) GetStatisticsDisplay(data []string) template.HTML {
+	f, _ := registry.GetFormatType(a.Format)
+
+	td := appointmentStatisticsTemplateStruct{
+		Text:   f.Format([]byte(a.Text)),
+		Dates:  make([]string, len(a.dates)),
+		Data:   make([]appointmentStatisticsTemplateStructInner, 0, len(data)),
+		Points: make([]float64, len(a.dates)),
+	}
+
+	for i := range a.dates {
+		td.Dates[i] = a.dates[i].Display
+	}
+
+	for d := range data {
+		var results map[string]string
+		err := json.Unmarshal([]byte(data[d]), &results)
+		if err != nil {
+			log.Printf("appointment: Error unmarshalling %d - %s - %s", d, data[d], err.Error())
+			continue
+		}
+
+		inner := appointmentStatisticsTemplateStructInner{
+			Name:    results[fmt.Sprintf("%s_name", a.id)],
+			Answers: make([][]string, len(a.dates)),
+		}
+
+		for i := range a.dates {
+			s := results[a.dates[i].ID]
+			inner.Answers[i] = []string{appointmentColour(s), s}
+			td.Points[i] += appointmentPoints(s)
+		}
+
+		td.Data = append(td.Data, inner)
+	}
+
+	bestPoints := math.Inf(-1)
+
+	for i := range td.Points {
+		if td.Points[i] > bestPoints {
+			bestPoints = td.Points[i]
+			td.Best = a.dates[i].Display
+			td.BestNumber = i
+		}
+	}
+
+	output := bytes.NewBuffer(make([]byte, 0))
+	err := appointmentStatisticsTemplate.Execute(output, td)
+	if err != nil {
+		log.Printf("date: Error executing template (%s)", err.Error())
+	}
+	return template.HTML(output.Bytes())
+}
+
+func (a appointment) GetDatabaseEntry(data map[string][]string) string {
+	results := make(map[string]string)
+	if len(data[fmt.Sprintf("%s_name", a.id)]) != 0 {
+		results[fmt.Sprintf("%s_name", a.id)] = data[fmt.Sprintf("%s_name", a.id)][0]
+	}
+	for i := range a.dates {
+		if len(data[a.dates[i].ID]) != 0 {
+			results[a.dates[i].ID] = data[a.dates[i].ID][0]
+		}
+	}
+	b, err := json.Marshal(results)
+	if err != nil {
+		return err.Error()
+	}
+	return string(b)
+}
